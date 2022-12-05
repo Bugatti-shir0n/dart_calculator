fb3d48cb954d6ba73528dfa8ef1a713318b68d66
Lynet_bits <shirulynn8@gmail.com>
2022-12-05 12:47:51 +0000

    A functional Calculator that prompts the user to input their operator

--- main.dart
+++ main.dart
@@ -1,17 +1,48 @@
 import "dart:math";
 import "dart:io";
 
-void main() {
-	print("Enter first number:");
-  double num1 = double.parse(stdin.readLineSync()!);
-  	print("Enter second number:");
-  double num2 = double.parse(stdin.readLineSync()!);
+String prompt(String promptText){
+  print("The prompt: ${promptText}");
+String answer = stdin.readLineSync()!;
+return answer;
+}
+double promptDouble(){
+  print("Enter a number: ");
+  double myNum = double.parse(stdin.readLineSync()!);
+  return myNum;
+}
+
+void main(){
+  double num1 = promptDouble();
+  double num2 = promptDouble();
+  String op = prompt("Enter an operation(+,-,/,*)");
+  if(op == '+'){
+    print(num1 + num2);
+  }
+  else if(op == '-') {
+    print(num1 - num2);
+  }
+  else if(op == '/'){
+    print(num1 / num2);
+  }
+  else if(op == '*'){
+    print(num1 * num2);
+  }
+  else{
+    print("Invalid Operator");
+  }
+}
+//void main() {
+	//print("Enter first number:");
+ // double num1 = double.parse(stdin.readLineSync()!);
+  //	print("Enter second number:");
+ // double num2 = //double.parse(stdin.readLineSync()!);
 
   
-  print(
-    num1 + num2
-  );
+  //print(
+  //  num1 + num2
+  //);
   //print(int.parse(num1) + int.parse(num2));
   
-}
+//}
 
